0aea0af1f15c8b4d0a60ad6362e64920d42dd553
naveed60 <advmiannaveedali@gmail.com>
2024-01-02 11:48:09 +0500

    sidebar component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import AuthInputs from './components/AuthInputs.jsx';
 import Header from './components/Header.jsx';
+// import Sidebar from './components/Sidebar.jsx';
 
 export default function App() {
   return (
     <>
+      {/* <Sidebar /> */}
       <Header />
       <main>
         <AuthInputs />
