6c6350814dc70130e70923671583fad04e54fbbf
naveed60 <advmiannaveedali@gmail.com>
2024-01-04 17:19:44 +0500

    modal added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-
 import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
 import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
+import Modal from './components/Modal'; // Import the Modal component
 import './App.css';
 
 const App = () => {
@@ -10,10 +10,16 @@ const App = () => {
     { text: 'Finish the course!', id: 'g2' }
   ]);
 
+  const [showModal, setShowModal] = useState(false);
+  const [modalContent, setModalContent] = useState({ inputValue: '' });
+
   const addGoalHandler = enteredText => {
     setCourseGoals(prevGoals => {
       const updatedGoals = [...prevGoals];
-      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      const newGoal = { text: enteredText, id: Math.random().toString() };
+      updatedGoals.unshift(newGoal);
+      setModalContent({ inputValue: enteredText });
+      setShowModal(true);
       return updatedGoals;
     });
   };
@@ -23,6 +29,10 @@ const App = () => {
       const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
       return updatedGoals;
     });
+  };
+
+  const closeModalHandler = () => {
+    setShowModal(false);
   };
 
   let content = (
@@ -37,19 +47,17 @@ const App = () => {
 
   return (
     <div>
+      {showModal && (
+        <Modal
+          inputValue={modalContent.inputValue}
+          onClose={closeModalHandler}
+        />
+      )}
       <section id="goal-form">
         <CourseInput onAddGoal={addGoalHandler} />
       </section>
-      <section id="goals">
-        {content}
-        {/* {courseGoals.length > 0 && (
-          <CourseGoalList
-            items={courseGoals}
-            onDeleteItem={deleteItemHandler}
-          />
-        ) // <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
-        } */}
-      </section>
+      <section id="goals">{content}</section>
+      
     </div>
   );
 };
